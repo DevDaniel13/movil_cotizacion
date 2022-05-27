94fab846bd9e62cfd0c2c03f324e8b9827688a7f
DevDaniel13 <81439840+DevDaniel13@users.noreply.github.com>
2022-05-27 01:47:27 -0600

    Add files via upload

--- src/main/java/com/example/cotizacion/Cotizacion.java
+++ src/main/java/com/example/cotizacion/Cotizacion.java
@@ -1,44 +1,37 @@
 package com.example.cotizacion;
 
 public class Cotizacion{
-    private int numCotizacion;
-    private String descripcionAuto;
+    private int id;
+    private String descripcion;
     private int precio;
-    private int porcentajeInicial;
+    private int porcentaje;
     private int plazo;
 
     public Cotizacion(){
 
     }
-    public Cotizacion(int numCotizacion, String descripcionAuto, int precio, int porcentajeInicial, int plazo){
-        this.numCotizacion=numCotizacion;
-        this.descripcionAuto=descripcionAuto;
-        this.precio=precio;
-        this.porcentajeInicial=porcentajeInicial;
-        this.plazo=plazo;
-    }
-    public Cotizacion(Cotizacion cotizacion){
-        System.out.println("Numero de cotizacion: "+this.numCotizacion);
-        System.out.println("Descripcion: "+this.descripcionAuto);
-        System.out.println("Precio: "+this.precio);
-        System.out.println("Porcentaje inicial: "+this.porcentajeInicial);
-        System.out.println("Plazo: "+this.plazo);
+    public Cotizacion(int id, String descripcion, int precio, int porcentaje, int plazo){
+        this.id = id;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.porcentaje = porcentaje;
+        this.plazo = plazo;
     }
 
-    public int getNumCotizacion() {
-        return numCotizacion;
+    public int getId() {
+        return id;
     }
 
-    public void setNumCotizacion(int numCotizacion) {
-        this.numCotizacion = numCotizacion;
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public String getDescripcionAuto() {
-        return descripcionAuto;
+    public String getDescripcion() {
+        return descripcion;
     }
 
-    public void setDescripcionAuto(String descripcionAuto) {
-        this.descripcionAuto = descripcionAuto;
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
     }
 
     public double getPrecio() {
@@ -49,12 +42,12 @@ public class Cotizacion{
         this.precio = precio;
     }
 
-    public int getPorcentajeInicial() {
-        return porcentajeInicial;
+    public int getPorcentaje() {
+        return porcentaje;
     }
 
-    public void setPorcentajeInicial(int porcentajeInicial) {
-        this.porcentajeInicial = porcentajeInicial;
+    public void setPorcentaje(int porcentaje) {
+        this.porcentaje = porcentaje;
     }
 
     public int getPlazo() {
@@ -66,21 +59,23 @@ public class Cotizacion{
     }
 
     public int calculoInicial(){
-        return this.precio*this.porcentajeInicial/100;
+        return this.precio*this.porcentaje/100;
     }
+
     public int totalFinanciar(){
         return this.precio-calculoInicial();
     }
+
     public int pagoMensual(){
         return totalFinanciar()/this.plazo;
     }
 
     @Override
     public String toString() {
-        return "Numero de cotizacion: " + numCotizacion +
-                "\nDescripcion: " + descripcionAuto + '\'' +
+        return "Numero de cotizacion: " + id +
+                "\nDescripcion: " + descripcion +
                 "\nPrecio: $" + precio +
-                "\nPorcentaje inicial: %" + porcentajeInicial +
+                "\nPorcentaje inicial: %" + porcentaje +
                 "\nPlazo (meses): " + plazo;
     }
 }
--- src/main/java/com/example/cotizacion/MainActivity.java
+++ src/main/java/com/example/cotizacion/MainActivity.java
@@ -9,17 +9,12 @@ import android.os.Bundle;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView lblNumCotizacion;
-    private TextView lblDescripcion;
-    private TextView lblPrecio;
-    private TextView lblPorcentaje;
-    private TextView lblPlazo;
-    private TextView lblDatos;
-    private EditText txtNumCotizacion;
+    private EditText txtNum;
     private EditText txtDescripcion;
     private EditText txtPrecio;
     private EditText txtPorcentaje;
     private EditText txtPlazo;
+    private TextView lblDatos;
     private Button btnCotizacion;
     private Button btnCerrar;
     private Button btnLimpiar;
@@ -45,39 +40,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lblNumCotizacion=(TextView) findViewById(R.id.lblNumCotizacion);
-        lblDescripcion=(TextView) findViewById(R.id.lblDescripcion);
-        lblPrecio=(TextView) findViewById(R.id.lblPrecio);
-        lblPorcentaje=(TextView) findViewById(R.id.lblPorcentaje);
-        lblPlazo=(TextView) findViewById(R.id.lblPlazo);
-        lblDatos=(TextView) findViewById(R.id.lblDatos);
-        txtNumCotizacion=(EditText) findViewById(R.id.txtNumCotizacion);
+        txtNum=(EditText) findViewById(R.id.txtNum);
         txtDescripcion=(EditText) findViewById(R.id.txtDescripcion);
         txtPrecio=(EditText) findViewById(R.id.txtPrecio);
         txtPorcentaje=(EditText) findViewById(R.id.txtPorcentaje);
         txtPlazo=(EditText) findViewById(R.id.txtPlazo);
+        lblDatos=(TextView) findViewById(R.id.lblDatos);
         btnCotizacion=(Button) findViewById(R.id.btnCotizacion);
         btnCerrar=(Button) findViewById(R.id.btnCerrar);
         btnLimpiar=(Button) findViewById(R.id.btnLimpiar);
-        lblNumCotizacion.setText("Capture el numero de cotizacion");
-        lblDescripcion.setText("Capture la descripcion del auto");
-        lblPrecio.setText("Capture el precio del auto");
-        lblPorcentaje.setText("Capture el porcentaje inicial del auto");
-        lblPlazo.setText("Capture el plazo (meses) del auto");
-
         btnCotizacion.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                int numCotizacion=Integer.parseInt(txtNumCotizacion.getText().toString());
-                String descripcion=txtDescripcion.getText().toString();
-                int precio= Integer.parseInt(txtPrecio.getText().toString());
-                int porcentaje=Integer.parseInt(txtPorcentaje.getText().toString());
-                int plazo=Integer.parseInt(txtPlazo.getText().toString());
-                Cotizacion cotizacion=new Cotizacion(numCotizacion,descripcion,precio,porcentaje,plazo);
-                lblDatos.setText(cotizacion.toString()+
-                        "\nPago inicial: $"+cotizacion.calculoInicial()+ "\n Total a financiar: $"+
-                        cotizacion.totalFinanciar()+"\n Pago mensual: $"+cotizacion.pagoMensual());
-
+                if(txtNum.getText().toString().matches("") || txtDescripcion.getText().toString().matches("") || txtPrecio.getText().toString().matches("") || txtPorcentaje.getText().toString().matches("") || txtPlazo.getText().toString().matches("")){
+                    Toast.makeText(MainActivity.this,"Existe un dato invalido",Toast.LENGTH_SHORT).show();
+                }else{
+                    int id=Integer.parseInt(txtNum.getText().toString());
+                    String descripcion=txtDescripcion.getText().toString();
+                    int precio= Integer.parseInt(txtPrecio.getText().toString());
+                    int porcentaje=Integer.parseInt(txtPorcentaje.getText().toString());
+                    int plazo=Integer.parseInt(txtPlazo.getText().toString());
+                    Cotizacion cotizacion=new Cotizacion(id,descripcion,precio,porcentaje,plazo);
+                    lblDatos.setText(cotizacion.toString()+
+                            "\nPago inicial: $"+cotizacion.calculoInicial()+ "\n Total a financiar: $"+
+                            cotizacion.totalFinanciar()+"\n Pago mensual: $"+cotizacion.pagoMensual());
+                }
             }
         });
         btnCerrar.setOnClickListener(new View.OnClickListener() {
@@ -87,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         });
         btnLimpiar.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                txtNumCotizacion.setText("");
+                txtNum.setText("");
                 txtDescripcion.setText("");
                 txtPrecio.setText("");
                 txtPorcentaje.setText("");
